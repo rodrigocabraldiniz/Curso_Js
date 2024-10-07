0bf68c760879783b956dab1ea15ae723e8ebecae
Rodrigo.Cabral.Diniz <101573317+rodrigocabraldiniz@users.noreply.github.com>
2024-10-07 20:45:09 -0300

    Add files via upload

---
+++ variaveisEconstantes.js
@@ -0,0 +1,13 @@
+//var, é uma variavel em que posso declara-la novamente sem problemas
+var a = 3
+var a = 40
+
+//let, é uma variavel, em que não posso declara-la novamente
+let b = 4
+//EX do que não pode acontecer: let b = 4
+//                              let b = 7
+
+//variavel constante (Não pode-se atribuir um novo valor)
+const c = 5
+
+console.log(a, b)
